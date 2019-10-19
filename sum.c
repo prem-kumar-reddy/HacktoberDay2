c3ca6ab4ea2bd32d268326cdb3927efdd8baa6a8
Prem Kumar <nesanurupremkumar123@gmail.com>
2019-10-19 19:23:47 +0530

    Sum

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+        int a=5,b=10;
+        printtf("Sum=%d",sum(a,b));
+        printf("Diff=%d",diff(a,b));
+        return 0;
+}
+int sum(int a,int b)
+{       return a+b;}
